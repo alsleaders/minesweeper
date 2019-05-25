d920c9c96e702e35dfd4bc86d114a2877282bd61
Amanda Leaders <alsleaders@gmail.com>
2019-05-25 19:08:33 -0400

    it actually brings back the new board!

--- src/components/Minesweeper.js
+++ src/components/Minesweeper.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
+let stringInterpolationIsBananas = 'https://minesweeper-api.herokuapp.com/games'
+
 class Minesweeper extends Component {
   state = {
-    startGame: []
+    startGame: [],
+    id: ''
   }
 
   componentDidMount() {
-    fetch('https://minesweeper-api.herokuapp.com/games', {
+    fetch(`${stringInterpolationIsBananas}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -24,29 +27,54 @@ class Minesweeper extends Component {
         console.log(game.board)
         console.log([game.board])
         this.setState({
+          startGame: game.board,
+          id: game.id
+        })
+      })
+  }
+
+  leftClickOnly = (rowdex, codex) => {
+    console.log(this.state.id)
+    console.log('did this work', rowdex, codex)
+    // make the left return an api call
+    fetch(`${stringInterpolationIsBananas}/${this.state.id}/check`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ row: rowdex, col: codex })
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(game => {
+        this.setState({
           startGame: game.board
         })
       })
   }
+  // does the flag on right click need to return an api call?
   render() {
-    // map through the columns
-    // map through the rows
-    // make each unit of table data an on click
-    // make each on click have a left and right
-    // make the left return an api call
-    // does the flag on right click need to return an api call?
     return (
+      // map through the columns - Done
       <table>
         <tbody>
-          {this.state.startGame.map((column, index) => {
-            console.log(index)
+          {this.state.startGame.map((column, codex) => {
+            console.log(codex)
             return (
-              <tr key={index}>
+              // map through the rows - Done
+              <tr key={codex}>
                 {this.state.startGame.map((row, rowdex) => {
                   console.log({ rowdex })
                   return (
-                    <td key={rowdex} id="game-boxes">
-                      {rowdex}
+                    // make each unit of table data an on click
+                    <td
+                      key={rowdex}
+                      id="game-boxes"
+                      // make each on click have a left and right
+                      onClick={() => this.leftClickOnly(codex, rowdex)}
+                    >
+                      {this.state.startGame[codex][rowdex]}
                     </td>
                   )
                 })}
